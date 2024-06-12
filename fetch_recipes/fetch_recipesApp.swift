e4b6028188b6fff9d6e078afb9089caff056a88e
jiabaow <jiabaowenwendy@gmail.com>
2024-06-11 18:43:15 -0700

    Initial Commit

---
+++ fetch_recipes/fetch_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_recipesApp.swift
+//  fetch_recipes
+//
+//  Created by 温嘉宝 on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct fetch_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
